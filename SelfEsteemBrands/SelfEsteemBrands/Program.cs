7beb5f3967ba3fdfa7cdca90fe74672af88caa0c
Joel Carrizales <joelcarrizales22@gmail.com>
2021-12-21 20:10:46 -0600

    Initial creation of console app, GitHubService file

---
+++ SelfEsteemBrands/SelfEsteemBrands/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfEsteemBrands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SelfEsteemBrands/SelfEsteemBrands/Services/GitHubService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfEsteemBrands.Services
+{
+    class GitHubService
+    {
+    }
+}

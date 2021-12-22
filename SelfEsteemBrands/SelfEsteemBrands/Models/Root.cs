d77b91247bd10be8e4e15551461a908bf73456ac
Joel Carrizales <joelcarrizales22@gmail.com>
2021-12-21 21:40:36 -0600

    initial creation of Controllers, Models, and Services

---
+++ SelfEsteemBrands/SelfEsteemBrands/Models/Root.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfEsteemBrands.Models
+{
+    public class Root
+    {
+        public int total_count { get; set; }
+        public bool incomplete_results { get; set; }
+        public List<GitHubRepo> items { get; set; }
+    }
+}
---
+++ SelfEsteemBrands/SelfEsteemBrands/Services/GitHubService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfEsteemBrands.Services
+{
+    public class GitHubService
+    {
+        private const string URL = "https://api.github.com/search/repositories?q=language:javascript stars:>10000 sort:stars-desc&per_page=5";
+
+        public GitHubService()
+        {
+
+        }
+    }
+}
